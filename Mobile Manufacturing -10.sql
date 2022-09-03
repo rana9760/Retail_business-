60b4beba5d5e8340862f5ceec5913e24a57ea077
rana9760 <91861866+rana9760@users.noreply.github.com>
2022-09-03 16:50:05 +0530

    Update SQL query

--- Mobile Manufacturing -10.sql
+++ Mobile Manufacturing -10.sql
@@ -1,3 +1,6 @@
+-- Question 1
+List All the states in which we have customer who have bought cellphones from 2005 till date ?
+
 
 --Answer 1
 
@@ -8,7 +11,9 @@ where Date >= '2005'
 order by date asc
 
 
+--- Question 2
 
+which state in the us buying more samsung cell phones ?
  --Answer 2
 
 select  Top 1 manufacturer_Name, Country,State ,Count(State)Max_User from DIM_MANUFACTURER a
@@ -19,6 +24,8 @@ where manufacturer_Name ='Samsung' and Country = 'US'
 group by manufacturer_Name, Country, State
 order by Count(State) desc
 
+-- Question 3
+Show the number transcation for each model per zip code per state?
 
 --Answer 3
 
@@ -28,12 +35,17 @@ join DIM_MODEL c on b.idmodel = c.idmodel
 group by Model_name,Zipcode,State,City
 order by 5 desc
 
+-- Question 4
+Show the cheapest cellphones ?
+
 --Answer 4
 
 select *from DIM_MANUFACTURER a
 join DIM_MODEL b on a.IDManufacturer = b.IDManufacturer
 where Unit_price = (select MIN(Unit_price)from DIM_MODEL)
 
+-- Question 5
+Find out the average price for each model in the top 5 manufacturer  in terms of sales quantity and order by average price ?
 
 --Answer 5
 
@@ -48,7 +60,8 @@ select *
 	where Rownum <=5               
 	order by AvgUnitPrice desc
 
-
+--Question 6 
+List the name of customer and the average amount spent in 2009 , where the average is higher than 500 ?
 --Answer 6
 
 select Customer_Name,Date,avg(TotalPrice)AMTspent from FACT_TRANSACTIONS a
@@ -57,7 +70,8 @@ where YEAR(Date) = '2009'
 group by Customer_Name,Date
 having avg(TotalPrice) > 500 
 
-
+-- Question 7
+List if there is any model that was in top 5 in terms of quantity , simultaneously in 2008 , 2009 and 2010 ?
 --Answer 7
 
 select a.Model_Name from (		  
@@ -85,7 +99,8 @@ select  c.Model_Name from (
 		  order by SUM(Quantity) desc
 		  )c
 
-
+---- Question 8
+-- Show the manufacturer with second top sales in the year 2009 and the manfacturer with 2 top sales in the year 2010 ?
 --Answer 8
 
 -- This is I have done with Union all but in this I have got record all in two column with same header. But othqr query i have used different way so that value not come in same column  it will come in different column.
@@ -129,7 +144,8 @@ select * from (
 
 where a.DRank = 2 and b.DRank = 2
 
-
+-- Question 9
+-- Show the manfacturer that sold cellphone in 2010 but didn't in 2009 ?
 --Answer 9
 
 select Model_Name,Date from FACT_TRANSACTIONS a
@@ -143,6 +159,7 @@ join DIM_MODEL b on a.IDModel = b.IDModel
 where  year(date) = '2009'
 
 
+-- Find top 100 customer and their average spend , average quantity by each year .Also find the percentage of change in their spend ?
 --Answer 10
 
 
